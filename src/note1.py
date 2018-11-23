4e4fe535d083910a0b0e65a6fec937becdb1c9ff
sunqi <sunqi1@cmcm.com>
2018-11-23 11:58:10 +0800

    Initial commit

---
+++ src/note1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+#coding=utf-8
+
+counter = 100  # 赋值整型变量
+miles = 1000.0  # 浮点型
+name = "John"  # 字符串
+a, b, c = 1, 2, "john"
+
+print counter,
+print miles,
+print name
+
+print "===================================="
+
+print a, b, c
